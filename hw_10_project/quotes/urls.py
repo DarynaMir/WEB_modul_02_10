3734786b27a046382aab39cdf3e2de928a41d53a
Daryna Mirzaholi <dmirzagholi@gmail.com>
2024-03-22 18:50:41 +0000

    first_commit

--- hw_10_project/quotes/urls.py
+++ hw_10_project/quotes/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 app_name = "quotes"
+
 urlpatterns = [
     path("", views.main, name="root"),
-    path("<int:page>", views.main, name="root_paginate"),
+    #path("<int:page>", views.main, name="root_paginate"),
 ]
--- hw_10_project/users/urls.py
+++ hw_10_project/users/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
+app_name = "users"
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("quotes.urls")),
+    #path("users/", include("users.urls")),
 ]
